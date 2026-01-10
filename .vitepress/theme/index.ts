dfa508b438db621196cb54bc2550fe18a3ab3326
WingChunWong <huangrongjun2200@outlook.com>
2026-01-10 18:16:28 +0800

    📦 build(theme): 重构样式文件目录结构

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,14 +4,14 @@ import { useRoute } from 'vitepress';
 import { onMounted, watch, nextTick } from 'vue';
 import mediumZoom from 'medium-zoom';
 
-import "./style.css";
-import "./kbd.css";
-import "./overall.css";
-import "./custom-block.css";
-import "./font.css";
-import "./medium-zoom.css";
-import "./code-block.css";
-import "./footer.css"
+import "./styles/style.css";
+import "./styles/kbd.css";
+import "./styles/overall.css";
+import "./styles/custom-block.css";
+import "./styles/font.css";
+import "./styles/medium-zoom.css";
+import "./styles/code-block.css";
+import "./styles/footer.css"
 
 export default {
   ...DefaultTheme,
